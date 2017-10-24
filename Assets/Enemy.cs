c85cd22b8b17c996611e1f8aabcae3f5af544d27
MV Coder <mvcoder@MVCC-MACMINI-4.local>
2017-10-23 18:43:03 -0700

    init

---
+++ Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject player;
+	NavMeshAgent agent;
+	public bool isSeen = false;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isSeen) {
+			agent.destination = transform.position;
+		} else {
+			agent.destination = player.transform.position;
+		}
+	}
+
+	void OnCollisionEnter(Collision col) {
+		GameObject player = col.collider.gameObject;
+		if (player.name == "Player") {
+			Rigidbody r = player.GetComponent<Rigidbody> ();
+			r.constraints = RigidbodyConstraints.None;
+			r.AddTorque (Camera.main.transform.eulerAngles*100, ForceMode.Impulse);
+		}
+	}
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class Move : MonoBehaviour {
+
+	public float moveSpeed = 10;
+	GameObject cam;
+	public GameObject enemy;
+
+	public ArrayList list;
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main.gameObject;
+
+		var dof = m_Profile.vignette.settings;
+		dof.intensity = 0;
+		m_Profile.vignette.settings = dof;
+	}
+
+	public PostProcessingProfile m_Profile;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z) * Time.deltaTime * moveSpeed);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (new Vector3(-cam.transform.right.x, 0, -cam.transform.right.z) * Time.deltaTime * moveSpeed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (new Vector3(-cam.transform.forward.x, 0, -cam.transform.forward.z) * Time.deltaTime * moveSpeed);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (new Vector3(cam.transform.right.x, 0, cam.transform.right.z) * Time.deltaTime * moveSpeed);
+		}
+		enemy.GetComponent<Enemy> ().isSeen = enemy.GetComponent<Renderer> ().isVisible;
+
+		if (Input.GetKey (KeyCode.V)) {
+
+			var dof = m_Profile.vignette.settings;
+			dof.intensity += .0001f;
+			m_Profile.vignette.settings = dof;
+		}
+	}
+
+	void OnApplicationQuit() {
+		var dof = m_Profile.vignette.settings;
+		dof.intensity = 0;
+		m_Profile.vignette.settings = dof;
+	}
+
+	bool IsGrounded() {
+		RaycastHit hit;
+		if (Physics.Raycast(new Ray(transform.position, -Vector3.up, 1.1), out hit)) {
+			return hit.collider.tag == "Floor";
+		}
+
+	}
+
+}
